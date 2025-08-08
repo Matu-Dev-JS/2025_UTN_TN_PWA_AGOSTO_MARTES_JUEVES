ca52e5c24d91b98e62102d1cd8ee059e5b19a047
matu.dev <mati.a.gimenez2002@gmail.com>
2025-08-07 21:10:03 -0300

    Clase 4

--- Clase-4/proyecto-ts/src/main.ts
+++ Clase-4/proyecto-ts/src/main.ts
@@ -25,6 +25,18 @@ class Persona {
 
 }
 
+class Superpersona extends Persona{
+  superpoder: string
+  constructor(nombre: string, apellido: string, superpoder: string){
+    super(nombre, apellido)
+    this.superpoder = superpoder
+  }
+}
+
+
+const homelander = new Superpersona('Homelander', 'Ni idea', 'Volar')
+
+homelander.presentarme()
 
 
 
@@ -56,6 +68,14 @@ Crear la class Tienda
     -Buscar el item en la lista de items, si existe lo va a eliminar y incrementara su cantidad de dinero en cuenta igual al precio del item
     -Si no hay item con ese id lanzar mensaje de error "Producto no encontrado"
   
+  -comprar(nuevo_item, cantidad)
+  
+
+  ItemTienda extends Item
+    -stock
+    -setStock(nuevo_stock) y va a guardar el nuevo stock como valor de stock
+
+
 */
 
 class Item {
@@ -69,6 +89,9 @@ class Item {
     this.id = id
     this.descripcion = descripcion
   }
+  mostrarItem(): void {
+    console.log(`${this.nombre} es ${this.descripcion} y cuesta $${this.precio} Rupias`)
+  }
 }
 
 class Tienda {
@@ -81,23 +104,44 @@ class Tienda {
 		this.cantidad_dinero_en_cuenta = cantidad_dinero_en_cuenta
 		this.id = id
 	}
+  buscarPorId(id_item_buscado: number): null | Item {
+    for (const item of this.items) {
+      if (item.id === id_item_buscado) {
+        return item
+      }
+    }
+    return null
+  }
+
+  eliminarPorId(id_item_buscado: number): boolean {
+    const itemIndex = this.items.findIndex(item => item.id === id_item_buscado);
+    if (itemIndex !== -1) {
+      this.items.splice(itemIndex, 1);
+      return true;
+    }
+    return false;
+  }
+  vender(id_item_vendido : number) : void {
+    const item = this.buscarPorId(id_item_vendido);
+    if (item) {
+      this.cantidad_dinero_en_cuenta += item.precio;
+      this.eliminarPorId(id_item_vendido);
+      console.log(`Se ha vendido ${item.nombre} por $${item.precio} Rupias. Dinero en cuenta: $${this.cantidad_dinero_en_cuenta} Rupias.`);
+    } else {
+      console.error("Producto no encontrado");
+    }
+  }
 
 }
 
-function retornarAlgo () : {nombre: string} | null {
-  if(Math.random( ) > 0.5){
-    return {nombre: 'pepe'}
-  }
-  return null
-}
-function hacerXAccion () {
-  let result = retornarAlgo()
-  if(!result){
-    console.log('Error')
-  }
-  else{
-    console.log(result.nombre)
-  }
-}
+const fravega = new Tienda('Fravega', 10000, 1)
 
-hacerXAccion()
+const tv_samsung = new Item('Tv Samsung 45"', 5000, 1, 'Tv muy buena')
+
+fravega.items.push(tv_samsung)
+
+fravega.vender(1)
+
+/* let cantidad_original = items.length
+filter
+this.items.length === cantidad_original */
