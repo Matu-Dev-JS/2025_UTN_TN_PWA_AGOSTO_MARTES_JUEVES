56e06750cf960c585602bac2fa4d2a7b90ffdc16
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-02 21:13:00 -0300

    clase 10

--- Clase-11-Nodejs/src/server.js
+++ Clase-11-Nodejs/src/server.js
@@ -2,12 +2,18 @@
 
 import ENVIRONMENT from "./config/environment.config.js";
 import connectMongoDB from "./config/mongoDB.config.js";
+import Users from "./models/User.model.js";
 
 connectMongoDB()
 
 
-
-
+/* 
+Users.insertOne({
+    name: 'pepe',
+    email: 'pepe@gmail.com',
+    password: 'pepe123',
+})
+ */
 
 
  
