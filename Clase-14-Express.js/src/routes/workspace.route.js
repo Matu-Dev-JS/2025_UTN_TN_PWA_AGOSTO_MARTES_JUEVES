ffe82f5bf14d564070e813d425d7cccce3835aed
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-11 22:04:05 -0300

    Clase 14

--- Clase-14-Express.js/src/routes/workspace.route.js
+++ Clase-14-Express.js/src/routes/workspace.route.js
@@ -62,32 +62,43 @@ workspace_router.get('/:workspace_id', async (request, response) => {
 //Este es el endpoint para crear workspaces
 workspace_router.post('/', async (request, response) => {
     try{
-
+        
         //request.body es donde esta la carga util enviada por el cliente
         //si aplicamos express.json() en nuestra app body siempre sera de tipo objeto
         const name = request.body.name
+        const url_img = request.url_img
         //Validar que name este y que sea valido (por ejemplo un string no VACIO de no mas de 30 caracteres)
         if(!name || typeof(name) !== 'string' || name.length > 30){
-            return response.json({
+            return response.status(400).json({
                 ok: false,
+                status: 400,
                 message: "el campo 'name' debe ser un string de menos de 30 caracteres"
+            })
+        }
+        else if(!url_img || typeof(url_img) !== 'string' ){
+            return response.status(400).json({
+                ok: false,
+                status: 400,
+                message: "el campo 'url_img' debe ser un string de menos de 30 caracteres"
             })
         }
         else{
             //Creamos el workspace con el repository
-            await WorkspacesRepository.createWorkspace(name, '')
+            await WorkspacesRepository.createWorkspace(name, url_img)
             //Si todo salio bien respondemos con {ok: true, message: 'Workspace creado con exito'}
-            return response.json({
+            return response.status(201).json({
                 ok: true,
+                status: 201,
                 message: 'Workspace creado con exito'
             })
         }
     }
     catch(error){
         console.log(error)
-        return response.json(
+        return response.status(500).json(
             {
                 ok: false,
+                status: 500,
                 message: 'Error interno del servidor'
             }
         )
