0159f3f01a1570b9a47f612c62587a9143ba1f23
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-16 20:00:31 -0300

    clase 15

---
+++ Clase-15-Express.js/src/models/User.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+
+//El esquema tiene los "contratos" de que es un User que luego podremos asignarselo a la coleccion de usuarios
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        created_at: {
+            type: Date, 
+            default: Date.now
+        },
+        modified_at: {
+            type: Date,
+            default: null
+        },
+        active: {
+            type: Boolean,
+            default: true
+        }
+    }
+)
+
+//Crear el modelo de User, cada accion que hagamos a la coleccion de User se hara por medio del modelo
+
+const Users = mongoose.model('User', userSchema)
+
+export default Users
